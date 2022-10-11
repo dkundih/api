5184c23f7d18ff6146b305740d3729aa07f8fc29
David Kundih <kundihdavid@gmail.com>
2022-10-12 01:09:42 +0200

    try

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:server
+web: gunicorn -w 4 -k uvicorn.worlers.UvicornWorker app:app
--- app.py
+++ app.py
@@ -1,47 +1,9 @@
-from math import degrees
-import dash
-from dash import html
-from dash import dcc
-from dash.exceptions import PreventUpdate
-from dash import Output, Input
-from threading import Timer
+from fastapi import FastAPI
 from datetime import datetime
-import json
-import requests
 
-#APP
-app = dash.Dash(__name__)
-server = app.server
-app.title = 'dkundih-promet'
+app = FastAPI()
 
-app.layout = html.Div([
-    
-    
-    html.Div([
-        dcc.Interval(
-            id = 'my_interval',
-            disabled = False,
-            interval = 1 * 5000,
-            n_intervals = 0,
-        )
-    ]),
-    
-    html.Div(id = 'output', children = [], style = {"font-size" : 16}),
-
-])
-
-@app.callback(
-    [Output('output', 'children')],
-    [Input('my_interval', 'n_intervals')]
-)
-def dataFeed(num):
-    if num == 0:
-        raise PreventUpdate
-    else:
-        time = datetime.now()
-        data = {"time" : time}
-        data = json.dumps(data, default=str)
-        return [data]
-
-if __name__ == '__main__':
-    app.run_server()
+@app.get("/")
+def home():
+    time = datetime.now()
+    return time
