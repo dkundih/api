e5c050377229aa2b844ae16d5749ef84ae7e3d87
David Kundih <kundihdavid@gmail.com>
2022-10-12 13:00:16 +0200

    cors test

--- feed_algo/provide_live_data.py
+++ feed_algo/provide_live_data.py
@@ -1,10 +1,11 @@
 from threading import Timer
 from datetime import datetime
 import requests
+import json
 
 def dataFeed():
     r = requests.get("https://dkundih-api.herokuapp.com/")
-    f = r.text
+    f = json.dumps(r.text)
     Timer(5.0, dataFeed).start()
     print(f)
     return f
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
-from datetime import datetime
-import json
+from fastapi import Request
+from fastapi.middleware.cors import CORSMiddleware 
+import time
+from starlette.middleware.base import BaseHTTPMiddleware
 
 app = FastAPI()
 
+class Middleware(BaseHTTPMiddleware):
+    async def dispatch(self, request :Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        response.headers["Time"] = str(process_time)
+        return response
+    
+
+origins = ["https://dkundih-api.herokuapp.com", "https://promet-kc.netlify.app", "https://localhost:3000", "https://localhost:8000", "https://localhost:5000", "https://localhost:8080"]
+
+app.add_middleware(Middleware)
+app.add_middleware(CORSMiddleware, allow_origins = origins)
+
 @app.get("/")
-def home():
-    time = datetime.now()
-    entry = {"Now" : time}
-    return entry
+async def hi():
+    return {"Hello" : "World"}
